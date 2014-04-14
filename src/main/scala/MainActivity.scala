538060f2fc05620188ce11b387969f9137604ae3
BrianHsu <brianhsu.hsu@gmail.com>
2014-04-14 14:33:41 +0800

    Implement basic custom widget class DialogFrame.

--- src/main/scala/MainActivity.scala
+++ src/main/scala/MainActivity.scala
@@ -10,16 +10,42 @@ import android.support.v4.view.PagerAdapter
 import android.view.View
 import android.view.ViewGroup
 import TypedResource._
+import idv.brianhsu.maidroid.ui.model._
 
 
-class MyView(context: Context, attrs: AttributeSet) extends LinearLayout(context, attrs) {
-  val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE).asInstanceOf[LayoutInflater]
-  inflater.inflate(R.layout.my_view, this, true)
+class DialogFrame(context: Context, attrs: AttributeSet) extends LinearLayout(context, attrs) {
+
+  private lazy val viewPager = this.findView(TR.dialogFrameViewPager)
+  private lazy val pagerIndicator = this.findView(TR.dialogFrameViewPagerIndicator)
+  private lazy val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE).
+                              asInstanceOf[LayoutInflater]
+
+  var adapterHolder: Option[MessageAdapter] = None
+  var messages: Vector[Message] = Vector.empty
+
+  private def createViewList(messages: List[Message]) = {
+
+    messages.map { message =>
+      val view = inflater.inflate(R.layout.dialog_item, null)
+      val textView = view.findView(TR.dialogItemTextView)
+      textView.setText(message.message)
+      view
+    }
+  }
+
+  def setMessages(messages: List[Message]) {
+    this.messages = messages.toVector
+    this.adapterHolder = Some(new MessageAdapter(createViewList(messages)))
+    this.adapterHolder.foreach { viewPager.setAdapter }
+    this.pagerIndicator.setViewPager(this.viewPager)
+  }
+
+  inflater.inflate(R.layout.dialog_frame, this, true)
 }
 
 import TypedResource._
 
-class ViewPagerAdapter(views: List[View]) extends PagerAdapter 
+class MessageAdapter(views: List[View]) extends PagerAdapter 
 {
   override def getCount = views.size
   override def instantiateItem(container: ViewGroup, position: Int): Object = {
@@ -34,31 +60,14 @@ class ViewPagerAdapter(views: List[View]) extends PagerAdapter
 
 class MainActivity extends Activity with TypedViewHolder
 {
-  private lazy val viewPager = findView(TR.viewpager)
-  private lazy val viewList = createViewList
-  private lazy val adapter = new ViewPagerAdapter(viewList)
+  private lazy val dialogFrame = findView(TR.dialogFrame)
 
   override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.main)
-    viewPager.setAdapter(adapter)
-    val pageIndicator = findView(TR.pageIndicator)
-    pageIndicator.setViewPager(viewPager)
+    dialogFrame.setMessages(Message(MaidMaro.Half.Happy, "QQQQQ") :: Message(MaidMaro.Half.Angry, "SSSS") :: Nil)
   }
 
-  def createViewList = {
-    val inflater = getLayoutInflater
-    val drawables = List("Hello World", "歡迎光臨", 
-      """|這是簡單的一個測試 1 AFda, daals,,這是測試，這是簡單的一個測試 2，中文怎麼栓啊。希望可以正常斷行，這是簡單的一個測試，這是簡單的一個測試 5這是簡單的一個測試 15""".stripMargin, "QQQQ", "DDD", "FFF"
-)
-
-    drawables.map { guidePic =>
-      val view = inflater.inflate(R.layout.item_view, null)
-      val message = view.findView(TR.messageContent)
-      message.setText(guidePic)
-      view
-    }
-  }
 
 
 }
