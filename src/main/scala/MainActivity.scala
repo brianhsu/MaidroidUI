e8662c776cb3fc2c7bfa98f0a5718759dce81aaa
BrianHsu <brianhsu.hsu@gmail.com>
2014-04-14 11:45:16 +0800

    Add basic layout.

--- src/main/scala/MainActivity.scala
+++ src/main/scala/MainActivity.scala
@@ -6,15 +6,18 @@ import android.content.Context
 import android.util.AttributeSet
 import android.widget.LinearLayout
 import android.view.LayoutInflater
+import android.support.v4.view.PagerAdapter
+import android.view.View
+import android.view.ViewGroup
+import TypedResource._
+
 
 class MyView(context: Context, attrs: AttributeSet) extends LinearLayout(context, attrs) {
   val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE).asInstanceOf[LayoutInflater]
   inflater.inflate(R.layout.my_view, this, true)
 }
 
-import android.support.v4.view.PagerAdapter
-import android.view.View
-import android.view.ViewGroup
+import TypedResource._
 
 class ViewPagerAdapter(views: List[View]) extends PagerAdapter 
 {
@@ -29,34 +32,33 @@ class ViewPagerAdapter(views: List[View]) extends PagerAdapter
   }
 }
 
-import TypedResource._
-
 class MainActivity extends Activity with TypedViewHolder
 {
-
   private lazy val viewPager = findView(TR.viewpager)
   private lazy val viewList = createViewList
   private lazy val adapter = new ViewPagerAdapter(viewList)
 
-  private var currentIndex = 0
-
   override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.main)
-    val titleIndicator = findView(TR.titles)
     viewPager.setAdapter(adapter)
-    titleIndicator.setViewPager(viewPager)
+    val pageIndicator = findView(TR.pageIndicator)
+    pageIndicator.setViewPager(viewPager)
   }
 
   def createViewList = {
     val inflater = getLayoutInflater
-    val drawables = List(R.drawable.guide1, R.drawable.guide2, R.drawable.guide3)
+    val drawables = List("Hello World", "歡迎光臨", 
+      """|這是簡單的一個測試 1 AFda, daals,,這是測試，這是簡單的一個測試 2，中文怎麼栓啊。希望可以正常斷行，這是簡單的一個測試，這是簡單的一個測試 5這是簡單的一個測試 15""".stripMargin, "QQQQ", "DDD", "FFF"
+)
+
     drawables.map { guidePic =>
       val view = inflater.inflate(R.layout.item_view, null)
-      val image = view.findView(TR.image)
-      image.setImageResource(guidePic)
+      val message = view.findView(TR.messageContent)
+      message.setText(guidePic)
       view
     }
   }
+
 
 }
